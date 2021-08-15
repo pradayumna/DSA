9d821d6e63d825443e9598f52d118584d55f7a78
Pradyumna Agrawal <pradyumna.agrawal@student.curtin.edu.au>
2021-08-15 19:06:03 +0530

    tower of hanoi code added. only exceptions and wrappers remaining now

--- Prac2/prac2.py
+++ Prac2/prac2.py
@@ -41,13 +41,59 @@ def gcd(a, b):
 
 #print(gcd(112, 120))
 
+def int2chr(n):
+    if n <= 9:
+        return chr(n + ord('0'))
+    else:
+        return chr(n - 10 + ord('A'))
+    
 def nconvert(n, base):
     if base > 16 or base < 2:
         return "choose a base between 2 and 16"
     if n < base:
-        ans = n
+        ans = int2chr(n)
     else:
-        ans = n%base + 10* nconvert(n//base, base)
+        ans = nconvert(n//base, base) + int2chr(n%base)
     return ans
 
-print(nconvert(34, 16))
+#print(nconvert(14782, 2))
+
+def moveDisk(src, dest, level):
+    print("\t" * (level) + "moving one disc from disc " + str(src) + "to " + str(dest))
+
+def towerOfHanoi(n, src, dest, level):
+    print("\t" * level + "towerOfHanoi(" + str(n) + "," + str(src) + "," + str(dest) + ')')
+    if n == 1:
+        moveDisk(src, dest, level)
+    else:
+        tmp = 6 - src - dest
+        towerOfHanoi(n-1, src, tmp, level+1)
+        moveDisk(src, dest, level+1)
+        towerOfHanoi(n-1, tmp, dest, level+1)
+        
+
+def main():
+    while(True):
+        print("G'day mate. Which program you wanna run today?\n(1) (F)actorial\n(2) (G)CD\n(3) (N)umber Converter\n(4) (T)ower of Hanoi\n(5) (E)xit\n")
+        menu = input().upper()
+        if menu == "F":
+            ans = factorial(int(input("Thats a great choice\nSo which factorial would you like to calculate?")))
+            print("Here is your answer " + str(ans) + "\n")
+        elif menu == "G":
+            num1 = int(input("enter first number"))
+            num2 = int(input("enter second number"))
+            ans = gcd(num1, num2)
+        elif menu == "N":
+            num1 = int(input("enter the base to which you want to convert"))
+            num2 = int(input("enter number in decimal"))
+            ans = nconvert(num2, num1)
+            print(str(num2) + " in " + str(num1) + " base is " + str(ans) + "\n")
+        elif menu == "T":
+            num = input("How many rings you want in the the game")
+            towerOfHanoi(int(num), 1, 3, 0)
+        elif menu == "E":
+            break
+        else:
+            print("Please enter a valid input as follow \n")
+if __name__ == "__main__":
+    main()
